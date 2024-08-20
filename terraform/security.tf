15853990f778cd26a8a99cc76477a1f32ca8c75d
MUSTAFA <142526337+MUSTAFA-3LI@users.noreply.github.com>
2024-08-21 02:59:39 +0300

    Update security.tf

--- terraform/security.tf
+++ terraform/security.tf
@@ -18,6 +18,22 @@ resource "aws_security_group" "allow_ssh" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+ingress {
+    description = "HTTP from anywhere"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP from anywhere"
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
