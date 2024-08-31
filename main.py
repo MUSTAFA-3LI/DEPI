0608a6ff21dcd7600c5f511b231001d69fc10790
MUSTAFA-3LI <zoro09402@gmail.com>
2024-08-31 15:22:33 +0300

    monitoring

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
+from flask import Flask, request
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__, static_url_path='/static')
+metrics = PrometheusMetrics(app)
 
 
 @app.route('/')
